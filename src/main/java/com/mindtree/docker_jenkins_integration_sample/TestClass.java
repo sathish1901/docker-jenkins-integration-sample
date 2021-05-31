ba4af702c7a677c671bfabc39044dd19d958b33f
Ubuntu <sathishkumar6mi@sathishkumar6mi.ekj5e0gvyfvu1kn0dttecth3bh.gx.internal.cloudapp.net>
2021-05-31 01:59:57 +0000

    NewFile added

---
+++ src/main/java/com/mindtree/docker_jenkins_integration_sample/TestClass.java
@@ -0,0 +1,11 @@
+package com.mindtree.docker_jenkins_integration_sample;
+
+public class TestClass {
+
+	
+	public static void main( String[] args )
+    {
+        System.out.println( "Hello Sathish!" );
+    }
+	
+}
